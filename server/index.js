debdb9ee3c0a078c4a58d7aeb11d638256e176d2
Adivyes <64081958+Adivyes@users.noreply.github.com>
2021-08-24 00:39:48 +0300

    init

--- server/index.js
+++ server/index.js
@@ -16,11 +16,20 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
-app.get('/',(req, res) => {res.send('Hello to Touri API')});
-const PORT = process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=> app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+
+.then(()=> app
+.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
 .catch((error)=> console.log(error.message));
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+  }
 
 mongoose.set('useFindAndModify', false);
