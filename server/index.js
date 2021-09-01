e06c647996758c89536496a00482901336d15a66
Adivyes <64081958+Adivyes@users.noreply.github.com>
2021-09-01 14:57:11 +0300

    init

--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
 // import path from 'path';
 
 const app = express();
-// dotenv.config();
+dotenv.config();
 
 app.use(bodyParser.json({limit:"30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
@@ -22,9 +22,11 @@ app.get('/',(req, res) => {
   res.send('Hello Touri');
 });
 
+const CONNECTION_URL = 'mongodb+srv://ASAppointment:ASAppointment1234@cluster0.zpdwy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=> app
 .listen(PORT, () => console.log(`server running on port: ${PORT}`)))
 .catch((error)=> console.log(error.message));
